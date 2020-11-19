eb459a7e5fa7bffb463154383a6eb39085b26666
Joao <joaoricardotg@gmail.com>
2020-11-19 18:21:10 -0300

    Atualizando backend

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,14 @@ const shutDown = require('./shutdown');
 
 //middlewares
 app.use(cors());
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 app.use(express.static('public'));
 app.use(express.json());
 //app.use(express.urlencoded({ extended: true }));
