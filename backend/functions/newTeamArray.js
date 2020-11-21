f6b104958dba4305f61160d3be43c96583287c9c
Joao <joaoricardotg@gmail.com>
2020-11-21 11:36:03 -0300

    Updating newTeamArray.js
    -Translating comments to english

--- backend/functions/newTeamArray.js
+++ backend/functions/newTeamArray.js
@@ -19,10 +19,10 @@ let newTeamArray = async () => {
     let playersArray = [];
     for (let i = 0; i < numPlayers; i++) {
         let newPlayer = {
-            name: playersNamesArray[i], //nome do jogador. Nomes sao fornecidos aleatoriamente pela API "names.drycodes"
-            age: parseInt(Math.random() * 22 + 18, 10), //idade do jogador entre 18 e 40 anos
-            numberPicks: parseInt(Math.random() * 20, 10), // numero de vezes que o jogador foi escolhido (entre 0 e 20 vezes)
-            nacionality: countriesArray[parseInt((countriesArray.length - 1) * Math.random(), 10)] // nacionalidade aleatoriamente escolhida do vetor fornecido pela API "REST Countries"
+            name: playersNamesArray[i], //player name. Names are randomly provided by the API "names.drycodes"
+            age: parseInt(Math.random() * 22 + 18, 10), //player age between 18 and 40 years
+            numberPicks: parseInt(Math.random() * 20, 10), // number of times the player was picked (between 0 and 20 vezes)
+            nacionality: countriesArray[parseInt((countriesArray.length - 1) * Math.random(), 10)] // player nacionality randomly provided by the API "REST Countries"
         }
         playersArray.push(newPlayer);
     }
